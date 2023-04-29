1301aedf36246daa0af7d7f5f513de5a3ae94ee1
masniloy <masaimunniloy88@gmail.com>
2023-04-30 02:42:33 +0600

    make a donor table

--- src/Pages/FiendDonor/FiendDonor.js
+++ src/Pages/FiendDonor/FiendDonor.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Donor from '../Donor/Donor';
 
 const FiendDonor = () => {
     return (
         <div className=' bg-red-100' >
 
-            <div className='my-20  lg:stats p-11'>
+            <div className='mt-20 mb-8 lg:stats p-11'>
                 <div className='mx-1 '>
-                    <select className="select select-bordered w-56 max-w-xs bg-red-100">
+                    <select className="select select-bordered w-56 max-w-xs bg-red-50">
                         <option disabled selected>Blood Group</option>
                         <option>A+</option>
                         <option>A-</option>
@@ -20,7 +21,7 @@ const FiendDonor = () => {
                 </div><br />
 
                 <div className='mx-1'>
-                    <select className="select select-bordered w-56 max-w-xs bg-red-100">
+                    <select className="select select-bordered w-56 max-w-xs bg-red-50">
                         <option disabled selected>District</option>
                         <option>Bagerhat</option>
                         <option>Chuadanga</option>
@@ -38,14 +39,14 @@ const FiendDonor = () => {
                 </div><br />
 
                 <div className='mx-1'>
-                    <select className="select select-bordered w-56 max-w-xs bg-red-100">
+                    <select className="select select-bordered w-56 max-w-xs bg-red-50">
                         <option disabled selected>Date</option>
 
                     </select>
                 </div><br />
 
                 <div className='mx-1'>
-                    <select className="select select-bordered w-56 max-w-xs bg-red-100">
+                    <select className="select select-bordered w-56 max-w-xs bg-red-50">
                         <option disabled selected>Status</option>
                         <option>All</option>
                         <option>Eligible</option>
@@ -53,11 +54,15 @@ const FiendDonor = () => {
                     </select>
                 </div><br />
                 <button className="btn bg-red-600 border-red-600 rounded-full mx-3 w-32">Search</button>
+
             </div>
 
-
+            <div className=''>
+                <Donor></Donor>
+            </div>
 
         </div>
+
     );
 };
 
