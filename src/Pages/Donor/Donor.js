956a78ac6057fde5d32dd20f972366d0c411ba7e
masniloy <masaimunniloy88@gmail.com>
2023-05-11 02:13:31 +0600

    Register page done

--- src/Pages/Donor/Donor.js
+++ src/Pages/Donor/Donor.js
@@ -3,17 +3,13 @@ import React from 'react';
 
 const Donor = () => {
     return (
-        <div >
+        <div className='' >
             <div className="overflow-x-auto lg:mx-11 pb-9">
                 <table className="table table-zebra w-full">
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
+
                             <th >Name</th>
                             <th >District</th>
                             <th >Blood group</th>
@@ -23,11 +19,7 @@ const Donor = () => {
                     <tbody>
                         {/* row 1 */}
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
+
                             <td>
                                 <div className="flex items-center space-x-3">
                                     <div className="avatar">
@@ -52,11 +44,7 @@ const Donor = () => {
                         </tr>
                         {/* row 2 */}
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
+
                             <td>
                                 <div className="flex items-center space-x-3">
                                     <div className="avatar">
@@ -81,11 +69,7 @@ const Donor = () => {
                         </tr>
                         {/* row 3 */}
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
+
                             <td>
                                 <div className="flex items-center space-x-3">
                                     <div className="avatar">
@@ -110,11 +94,7 @@ const Donor = () => {
                         </tr>
                         {/* row 4 */}
                         <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
+
                             <td>
                                 <div className="flex items-center space-x-3">
                                     <div className="avatar">
@@ -137,12 +117,37 @@ const Donor = () => {
                                 <button className="btn bg-red-600 btn-xs border-red-600 px-4">Contact</button>
                             </th>
                         </tr>
+                        {/* row 4 */}
+                        <tr>
+
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">Md Shariar Ahamed</div>
+                                        <div className="text-sm opacity-50 text-red-600 font-bold ">Eligible</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Dhaka
+
+                            </td>
+                            <td className='font-bold'>B+</td>
+                            <th>
+                                <button className="btn bg-red-600 btn-xs border-red-600 px-4">Contact</button>
+                            </th>
+                        </tr>
 
                     </tbody>
                     {/* foot */}
                     <tfoot>
                         <tr>
-                            <th></th>
+
                             <th>Name</th>
                             <th>District</th>
                             <th>Blood Group</th>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,14 +1,65 @@
 import React from 'react';
-import inbuild from '../../New folder/image/In Build.gif'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faGoogle, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Register = () => {
     return (
-        <div>
-            <div className='flex h-fit  w-full justify-center items-center'>
-                <img src={inbuild} className="max-w-sm rounded-lg " /><br />
+        <div className=' stats '>
+            <div className="hero min-h-screen ">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center ">
+                        <h1 className="text-5xl font-bold mb-5">Register now!</h1>
+                        <p className="py-6 lg:px-6">Though Bangladesh has more than 160 million people, the number of safe blood bank is very few. Without divisional towns, there is hardly any blood bank. But a huge amount of blood is needed for treatment purposes. A good number of accidents take place every day where blood needs essentially.</p>
 
+                        <div className=' px-5  lg:stats gap-px  w-full'>
+                            <div className="form-control mt-6 lg:w-64">
+                                <button className="btn bg-red-600 border-red-600 rounded-full">Google <FontAwesomeIcon className=' h-6 ml-4' icon={faGoogle} /></button>
+                            </div>
+                            <div className="form-control mt-6 lg:w-64">
+                                <button className="btn bg-sky-600 border-sky-600 rounded-full">Facebook <FontAwesomeIcon className=' h-6 ml-4' icon={faFacebook} /></button>
+                            </div>
+                            <div className="form-control mt-6 lg:w-64">
+                                <button className="btn  bg-sky-700 border-sky-700 rounded-full">Linkedin <FontAwesomeIcon className=' h-6 ml-4' icon={faLinkedin} /></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-semibold">Name</span>
+                                </label>
+                                <input type="text" placeholder="name" className="input input-bordered bg-red-50 rounded-full" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-semibold">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered bg-red-50 rounded-full" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-semibold">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered bg-red-50 rounded-full " />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover ">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn bg-red-600 border-red-600 rounded-full">Register</button>
+                            </div>
+                            <div>
+                                <label className="label mt-4">
+                                    <span className="label-text ">I have an account !</span><Link to='/Register' className=' text-red-600'>Login now</Link>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <h1 className='text-2xl font-bold'>Register page is not ready yet !</h1>
         </div>
     );
 };
